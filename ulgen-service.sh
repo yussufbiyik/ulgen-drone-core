ded58a86137ac3f665b0da31f5cd6e910351b369
Yusuf Bıyık <yussuf.jpeg@gmail.com>
2025-08-19 10:53:25 +0300

    dosya adresi değiştirildi

--- ulgen-service.sh
+++ ulgen-service.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 source "$HOME/miniconda3/etc/profile.d/conda.sh"
 conda activate mavsdk
-cd "$HOME/ulgen/drone-core" || exit 1
+cd "$HOME/drone-core" || exit 1
 
 # Güncellemeleri al
 git pull
 
 # Servis scriptini çalıştır
-exec python "$HOME/ulgen/drone-core/service.py"
+exec python "$HOME/drone-core/service.py"
+
+# sudo mkdir -p ~/.ssh && sudo mv Desktop/id_ed25519 ~/.ssh && chmod 600 ~/.ssh/id_ed25519 && cd drone-core && git checkout sartname_gorevleri && 
