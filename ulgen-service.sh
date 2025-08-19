2c4d21dbfaa5364b9e3108c59cf1fcc0fc2d35fa
Yusuf Bıyık <yussuf.jpeg@gmail.com>
2025-08-19 12:22:54 +0300

    Komutlar eklendi

--- ulgen-service.sh
+++ ulgen-service.sh
@@ -6,9 +6,10 @@ cd "$HOME/drone-core" || exit 1
 # Güncellemeleri al
 git reset --hard
 git pull
+chmod +x "$HOME/drone-core/ulgen-service.py"
 
 # Servis scriptini çalıştır
 exec python "$HOME/drone-core/service.py"
 
-# testinggg
-# sudo mkdir -p ~/.ssh && sudo mv Desktop/id_ed25519 ~/.ssh && chmod 600 ~/.ssh/id_ed25519 && cd drone-core && git checkout sartname_gorevleri && 
+# sudo mkdir -p ~/.ssh && sudo chmod 700 ~/.ssh && sudo touch ~/.ssh/known_hosts && sudo chmod 600 ~/.ssh/known_hosts && sudo chown pi:pi ~/.ssh ~/.ssh/known_hosts && sudo mv Desktop/id_ed25519 ~/.ssh && sudo chmod 600 ~/.ssh/id_ed25519 && git clone git@github.com:ULGEN-Suru-IHA-Takimi/drone-core.git && cd drone-core && git checkout sartname_gorevleri 
+# cd drone-core && conda activate mavsdk && python service.py
