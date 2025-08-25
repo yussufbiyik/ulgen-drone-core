e216ca61c83a5f938a7df8183cee90fbbe114625
Yusuf Bıyık <yussuf.jpeg@gmail.com>
2025-08-26 01:31:29 +0300

    Servise wifi olmadan da çalışma desteği ekle

--- ulgen-service.sh
+++ ulgen-service.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Wifi bağlantısını bekle
+# 30 saniye için wifi bağlantısını bekle
+timeout=30
+interval=1
+elapsed=0
 until ping -c1 8.8.8.8 &>/dev/null; do
     echo "Wifi bekleniyor..."
-    sleep 5
+    sleep $interval
+    elapsed=$((elapsed + interval))
+    if [ $elapsed -ge $timeout ]; then
+        echo "30 saniye geçti, wifi olmadan devam ediliyor..."
+        break
+    fi
 done
-echo "Wifi bağlandı!"
 
 cd "$HOME/drone-core" || exit 1
 
@@ -14,9 +21,7 @@ git reset --hard
 git pull
 
 # Servis scriptini çalıştır
-source "$HOME/miniconda3/etc/profile.d/conda.sh"
-conda activate mavsdk
-exec python "$HOME/drone-core/service.py"
+python "$HOME/drone-core/service.py"
 
 # sudo mkdir -p ~/.ssh && sudo chmod 700 ~/.ssh && sudo touch ~/.ssh/known_hosts && sudo chmod 600 ~/.ssh/known_hosts && sudo chown pi:pi ~/.ssh ~/.ssh/known_hosts && sudo mv Desktop/id_ed25519 ~/.ssh && sudo chmod 600 ~/.ssh/id_ed25519 && git clone git@github.com:ULGEN-Suru-IHA-Takimi/drone-core.git && cd drone-core && git checkout sartname_gorevleri 
 # cd drone-core && conda activate mavsdk && python service.py
