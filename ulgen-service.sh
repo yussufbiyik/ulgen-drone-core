24d2ed2a623589640eab0a5889e3fdf5521f193b
Yusuf Bıyık <yussuf.jpeg@gmail.com>
2025-08-25 23:35:47 +0300

    Servis güncellemesi

--- ulgen-service.sh
+++ ulgen-service.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-source "$HOME/miniconda3/etc/profile.d/conda.sh"
-conda activate mavsdk
+
+# Wifi bağlantısını bekle
+until ping -c1 8.8.8.8 &>/dev/null; do
+    echo "Wifi bekleniyor..."
+    sleep 5
+done
+echo "Wifi bağlandı!"
+
 cd "$HOME/drone-core" || exit 1
 
 # Güncellemeleri al
 git reset --hard
 git pull
-chmod +x "$HOME/drone-core/ulgen-service.py"
 
 # Servis scriptini çalıştır
+source "$HOME/miniconda3/etc/profile.d/conda.sh"
+conda activate mavsdk
 exec python "$HOME/drone-core/service.py"
 
 # sudo mkdir -p ~/.ssh && sudo chmod 700 ~/.ssh && sudo touch ~/.ssh/known_hosts && sudo chmod 600 ~/.ssh/known_hosts && sudo chown pi:pi ~/.ssh ~/.ssh/known_hosts && sudo mv Desktop/id_ed25519 ~/.ssh && sudo chmod 600 ~/.ssh/id_ed25519 && git clone git@github.com:ULGEN-Suru-IHA-Takimi/drone-core.git && cd drone-core && git checkout sartname_gorevleri 
